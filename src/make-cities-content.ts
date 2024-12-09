1263f0e58942c38d448fa287d6a08acaeb2b1a28
dev-you-br <dev.you.br@gmail.com>
2024-12-09 01:22:17 -0500

    wip

--- src/make-cities-content.ts
+++ src/make-cities-content.ts
@@ -1,35 +1,14 @@
 import axios from 'axios'
 import { readFileSync, writeFileSync } from 'fs'
-import { zip } from 'lodash-es'
 import { citiesSchema, City } from './schema.js'
-
-type CityWithLlama = {
-  name: string
-  nameNormalized: string
-  llama: string
-}
-
 const citiesJsonPath = './resources/cities.json'
 const outputDir = './resources/content'
 const llamaUrl = 'http://localhost:11434/api/generate'
-const promptTemplate = `
-Crie um roteiro turístico bem detalhado e engajado para a cidade de {{NAME}}-{{REGION}}, Brasil, seguindo estas especificações:
 
-Introdução:
-Apresente a cidade destacando sua importância histórica e cultural.
-
-Atrações turísticas:
-Inclua cinco atrações turísticas, sendo algumas gratuitas e outras pagas (inclua o valor quando pagas). Descreva cada atração detalhadamente, abordando o que a torna especial e o que os visitantes podem esperar ao visitá-la.
-
-Restaurantes:
-Apresente três restaurantes, com uma variedade de estilos com pratos típicos da culinária local. Sendo dois restaurante acessíveis e um sofisticado.
-
-Dicas de viagem:
-Aborde temas como a melhor época para visitar e transporte na cidade.
-
-Responda em html embutido sem os elementos <html>, <head> ou <body> pois o resultado será inserido em um página existente.
-Responda em html apenas com os elementos: <h1>, <h2>, <h3> e <p>.
-` as const
+const proptResponseInHtml = `
+Responda em HTML.
+Utilize apenas os elementos: <h1>, <h2> e <p>.
+Exclua elementos externos como <html>, <head>, ou <body>, pois o conteúdo será inserido em uma página existente.`
 
 console.log(`Reading cities from ${citiesJsonPath}. Writtig to ${outputDir}`)
 
@@ -37,59 +16,98 @@ const cities = citiesSchema.parse(
   JSON.parse(readFileSync(citiesJsonPath, 'utf-8')),
 )
 
-const llamas: Array<string> = []
-for (const c of cities) {
-  const response = await generateLlama(c)
-  llamas.push(response)
+for (const city of cities) {
+  const content: Array<string> = []
+  content.push(await generateIntro(city))
+  content.push(await generateAtractions(city))
+  content.push(await generateRestaurants(city))
+  content.push(await generateHints(city))
+  writeContent(city, content.join('\n'))
 }
-
-zip(cities, llamas).map(toCityWithLlama).forEach(writeContent)
 
 console.log('Done!')
 
-function toCityWithLlama(
-  zip: [City | undefined, string | undefined],
-): CityWithLlama {
-  return {
-    name: zip[0]?.name ?? '',
-    nameNormalized: zip[0]?.nameNormalized ?? '',
-    llama: zip[1] ?? '',
+async function generateIntro(city: City) {
+  const prompt = `
+  Escreva uma introdução de dois parágrafos para a cidade de ${city.name}-${city.region} para um roteiro turístico.
+  ${proptResponseInHtml}
+  `
+  return await generateLlamaRetry(prompt)
+}
+
+async function generateAtractions(city: City) {
+  const prompt = `
+  Descreva cinco atrações turísticas para a cidade de ${city.name}-${city.region}, sendo algumas gratuitas e outras pagas.
+  Escreva três parágrafos para cada atração abordando o que a torna especial e o que os visitantes podem esperar ao visitá-la.
+  No último parágrafo indique se a atração é gratuíta ou paga e qual os dias e horário para visitação.
+  ${proptResponseInHtml}
+  `
+  return await generateLlamaRetry(prompt)
+}
+
+async function generateRestaurants(city: City) {
+  const prompt = `
+  Descreva três restaurantes para a cidade de ${city.name}-${city.region}, com uma variedade de estilos.
+  Inclua dois restaurantes acessíveis, com pratos típicos e deliciosos da culinária local, e um restaurante sofisticado que ofereça uma experiência mais requintada.
+  ${proptResponseInHtml}
+  `
+  return await generateLlamaRetry(prompt)
+}
+
+async function generateHints(city: City) {
+  const prompt = `
+  Descreva dicas de viagem para a cidade de ${city.name}-${city.region} para um roteiro turístico.
+  Sugira a melhor época para visitar a cidade, considerando clima, eventos ou festivais especiais.
+  Dê dicas sobre transporte local.
+  ${proptResponseInHtml}
+  `
+  return await generateLlamaRetry(prompt)
+}
+
+async function generateLlamaRetry(prompt: string) {
+  const llama = await generateLlama(prompt)
+  const llamaClean = clearLlama(llama)
+
+  if (
+    llamaClean.includes('html') ||
+    !llamaClean.includes('<h1>') ||
+    !llamaClean.includes('<p>')
+  ) {
+    console.log('Llama result does not look like the html we want.', llamaClean)
+    return await generateLlama(prompt)
   }
+  return llama
 }
 
-function writeContent(city: CityWithLlama): void {
-  const cityContent = `<div>${city.llama}</div>`
-  const outputPath = `${outputDir}/${city.nameNormalized}.html`
-  writeFileSync(outputPath, cityContent)
+function clearLlama(text: string) {
+  if (text.includes('```html')) {
+    return clearLlama(
+      text.substring(text.indexOf('```html') + 7, text.lastIndexOf('```')),
+    )
+  }
+  if (text.includes('<body>')) {
+    return clearLlama(
+      text.substring(text.indexOf('<body>') + 6, text.indexOf('</body>')),
+    )
+  }
+  return text
 }
 
-async function generateLlama(city: City): Promise<string> {
+async function generateLlama(prompt: string): Promise<string> {
   console.log(
-    `Asking Llama to generate content for ${city.name}-${city.region}`,
+    `Asking Llama to generate content for:
+    ${prompt}`,
   )
-  const prompt = promptTemplate
-    .replace('{{NAME}}', city.name)
-    .replace('{{REGION}}', city.region)
-
   const data = {
     model: 'llama3.2',
     prompt,
     stream: false,
   }
   const response = await axios.post(llamaUrl, data)
-  const llamaResult = response.data.response as string
-  console.log(`Llama responded with: ${llamaResult}`)
+  return response.data.response as string
+}
 
-  if (!llamaResult.includes('<h1>') && !llamaResult.includes('<p>')) {
-    console.log('Llama result does not look like html, re-generating')
-    return await generateLlama(city)
-  }
-
-  if (llamaResult.includes('```html')) {
-    return llamaResult.substring(
-      llamaResult.indexOf('```html') + 7  ,
-      llamaResult.lastIndexOf('```'),
-    )
-  }
-  return llamaResult
+function writeContent(city: City, content: string): void {
+  const outputPath = `${outputDir}/${city.nameNormalized}.html`
+  writeFileSync(outputPath, content)
 }

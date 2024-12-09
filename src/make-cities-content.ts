fdb28c0b9cc05977bb69e935c38d5136c533968b
dev-you-br <dev.you.br@gmail.com>
2024-12-09 09:29:51 -0500

    wil

--- src/make-cities-content.ts
+++ src/make-cities-content.ts
@@ -7,7 +7,7 @@ const llamaUrl = 'http://localhost:11434/api/generate'
 
 const proptResponseInHtml = `Responda em HTML.
 Utilize apenas os elementos: <h1>, <h2> e <p>.
-Exclua elementos externos como <html>, <head>, ou <body>, pois o conteúdo será inserido em uma página existente.`
+Exclua elementos externos como <html>, <head>, ou <body>, pois o conteúdo será inserido em uma página existente.`.trim()
 
 console.log(`Reading cities from ${citiesJsonPath}. Writtig to ${outputDir}`)
 
@@ -28,21 +28,21 @@ console.log('Done!')
 
 async function generateIntro(city: City) {
   const prompt = `Escreva uma introdução de dois parágrafos para a cidade de ${city.name}-${city.region} para um roteiro turístico.
-  ${proptResponseInHtml}`
+  ${proptResponseInHtml}`.trim()
   return await generateLlamaRetry(prompt)
 }
 
 async function generateAtractions(city: City) {
   const prompt = `Descreva cinco atrações turísticas para de ${city.name}-${city.region}, sendo algumas gratuitas e outras pagas.
   Escreva três parágrafos: primeiro descreve o que a torna especial; segundo o que os visitantes podem esperar ao visitá-la; terceiro se a atração é gratuíta ou paga e qual os dias e horário para visitação.
-  ${proptResponseInHtml}`
+  ${proptResponseInHtml}`.trim()
   return await generateLlamaRetry(prompt)
 }
 
 async function generateRestaurants(city: City) {
   const prompt = `Descreva três restaurantes para a cidade de ${city.name}-${city.region}, com uma variedade de estilos.
   Inclua dois restaurantes acessíveis, com pratos típicos da culinária local, e um restaurante sofisticado que ofereça uma experiência mais requintada.
-  ${proptResponseInHtml}`
+  ${proptResponseInHtml}`.trim()
   return await generateLlamaRetry(prompt)
 }
 
@@ -50,7 +50,7 @@ async function generateHints(city: City) {
   const prompt = `Descreva dicas de viagem para a cidade de ${city.name}-${city.region} para um roteiro turístico.
   Sugira a melhor época para visitar a cidade, considerando clima, eventos ou festivais especiais.
   Dê dicas sobre transporte local.
-  ${proptResponseInHtml}`
+  ${proptResponseInHtml}`.trim()
   return await generateLlamaRetry(prompt)
 }
 
